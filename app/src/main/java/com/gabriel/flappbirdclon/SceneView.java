462cefffc18de72254766c942769b1b8aaeb3504
Gabriel <>
2018-06-09 13:18:32 +0300

    - centered the char - added gravitation fall to the char

--- app/src/main/java/com/gabriel/flappbirdclon/SceneView.java
+++ app/src/main/java/com/gabriel/flappbirdclon/SceneView.java
@@ -11,6 +11,7 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.nio.charset.CharacterCodingException;
@@ -51,37 +52,10 @@ public class SceneView extends View {
     }
 
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        this.mViewWidth = w;
-        this.mViewHeight = h;
-        createBackgroundImage(w, h);
-
-
-        super.onSizeChanged(w, h, oldw, oldh);
-
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-
-
-
-
-        canvas.drawBitmap(bgBitmap,src, dst,null);
-
-        sprite.draw(canvas);
-
-        postInvalidateOnAnimation();
-    }
-
-
     private void init(){
 
         spritesBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.running_grant);
         sprite = new Sprite(this,spritesBitmap);
-
-
     }
 
     private void createBackgroundImage(int w, int h) {
@@ -97,9 +71,58 @@ public class SceneView extends View {
 
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        this.mViewWidth = w;
+        this.mViewHeight = h;
+        createBackgroundImage(w, h);
+
+        super.onSizeChanged(w, h, oldw, oldh);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        canvas.drawBitmap(bgBitmap,src, dst,null);
+
+        sprite.draw(canvas);
+
+        postInvalidateOnAnimation();
+    }
+
+
+
+
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        if (event.getAction() == MotionEvent.ACTION_DOWN ||
+//                event.getAction() == MotionEvent.ACTION_MOVE) {
+//            touchedX = event.getX();
+//            touchedY = event.getY();
+//            isTouched = true;
+//            postInvalidateOnAnimation();
+//            return true;
+//        } else {
+//            isTouched = false;
+//        }
+//        // will trigger a new call to onDraw()
+//        postInvalidateOnAnimation();
+//        return super.onTouchEvent(event);
+//    }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     public class Sprite {
+
+
+        protected static final int GRAVITY = 1;
+        protected static final int MAX_DROP_SPEED = 12;
+        protected int acceleration = GRAVITY;
+        protected int verticalSpeed;
 
         private Bitmap bitmap;
         private int currentFrame = 0;
@@ -110,7 +133,9 @@ public class SceneView extends View {
         private int x = 0;
         private int y = 0;
         private int xSpeed = 5;
+        private int ySpeed = 5;
         private SceneView sceneView;
+        private boolean freshScene = true;
 
 
         public Sprite(SceneView sceneView, Bitmap bitmap) {
@@ -118,24 +143,44 @@ public class SceneView extends View {
             this.bitmap = bitmap;
             this.width = (bitmap.getWidth() - 64) / 12;
             this.height = (bitmap.getHeight() - 292) / 6;
+
         }
 
 
         private void update() {
-            if (x > sceneView.getWidth() - width - xSpeed) {
-                xSpeed = -5;
+            //this is to init the bird in the first position
+            if (freshScene){
+                x = (sceneView.getWidth() / 2) - (width / 2);
+                y = (sceneView.getHeight() / 2) - (height / 2);
+                freshScene = false;
             }
-            if (x + xSpeed < 0) {
-                xSpeed = 5;
+
+//            if (x > sceneView.getWidth() - width - xSpeed) {
+//                xSpeed = -5;
+//            }
+//            if (x + xSpeed < 0) {
+//                xSpeed = 5;
+//            }
+//            x = x + xSpeed;
+
+            // now calculate the new speed
+            acceleration += GRAVITY; // always applying gravity to current acceleration
+            verticalSpeed += acceleration; // always applying the current acceleration tp the current speed
+            verticalSpeed = Math.min(verticalSpeed, MAX_DROP_SPEED); // but capping it to a terminal velocity (science bitch)
+
+            if (y > sceneView.getBottom() - height - ySpeed){
+                y = (sceneView.getHeight() / 2) - (height / 2);
             }
-            x = x + xSpeed;
-//            y = y + xSpeed;
+            y = y + verticalSpeed;
 
             currentFrame = (currentFrame + 1) % BMP_COLUMNS;
         }
 
         public void draw(Canvas canvas) {
+
             update();
+
+
             int srcX = currentFrame * width;
             int srcY = 1 * height;
             Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
