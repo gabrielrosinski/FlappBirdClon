20f9c2b7859a80355e8545297399fc88f2f9417b
Gabriel <>
2018-07-21 17:05:03 +0300

    - Moved Sprite to Class

--- app/src/main/java/com/gabriel/flappbirdclon/SceneView.java
+++ app/src/main/java/com/gabriel/flappbirdclon/SceneView.java
@@ -13,6 +13,8 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.gabriel.flappbirdclon.Workers.Sprite;
+
 import java.util.Random;
 
 public class SceneView extends View {
@@ -179,133 +181,6 @@ public class SceneView extends View {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    public class Sprite {
-
-
-        protected static final int GRAVITY = 1;
-        protected static final int MAX_DROP_SPEED = 12;
-        protected static final int MAX_JUMP_SPEED = -12;
-
-
-        private Bitmap bitmap;
-        private int currentFrame = 0;
-        private int width;
-        private int height;
-        private int x = 0;
-        private int y = 0;
-        private SceneView sceneView;
-        private boolean freshScene = true;
-        private boolean spriteTouched = false;
-
-        private static final int BMP_ROWS = 3;
-        private static final int BMP_COLUMNS = 3;
-        int velocity = 0;
-
-
-
-
-        public Sprite(SceneView sceneView, Bitmap bitmap) {
-            this.sceneView = sceneView;
-            this.bitmap = bitmap;
-
-            this.width = (bitmap.getWidth() ) / BMP_COLUMNS;
-            this.height = (bitmap.getHeight() ) / BMP_ROWS;
-
-        }
-
-
-
-        private boolean isCollisionDetected(Pipe pipe) {
-
-            if (pipe == null) {
-                Log.d("SceneView","Pipe must not be null");
-                throw new IllegalArgumentException("Pipe mut be not null");
-            }
-
-            int spritesWidth = (int) (width * 1.7);
-            int spritesHight = (int) (height * 1.4);
-            Rect spriteRect = new Rect(x, y, spritesWidth, spritesHight);
-            Rect pipeRect = pipe.getPipeRect();
-
-
-            if (pipe.pipeType == pipe.pipeType.UPPER){
-                if (spriteRect.right > pipeRect.left && spriteRect.left < pipeRect.left && spriteRect.top < pipeRect.bottom){
-//                    Log.d("SceneView","COliisoin");
-                    return true;
-                }
-            }else{
-
-                if (spriteRect.right > pipeRect.left && spriteRect.left < pipeRect.left && spriteRect.top > pipeRect.top){
-//                    Log.d("SceneView","COliisoin");
-                    return true;
-                }
-            }
-
-            //Touched the ground - Fail
-            if (spriteRect.top > sceneView.getBottom()){
-//                    Log.d("SceneView","COliisoin");
-                return true;
-            }
-
-            return false;
-        }
-
-
-
-
-
-
-
-        public void spriteTouched(){
-            this.spriteTouched = true;
-        }
-
-
-        private void update() {
-            //this is to init the bird in the first position
-            if (freshScene){
-                x = (sceneView.getWidth() / 2) - (width / 4);
-                y = (sceneView.getHeight() / 2) - (height / 2);
-                freshScene = false;
-            }
-
-
-            if ( y < sceneView.getTop()){
-//                y = (sceneView.getHeight() / 2) - (height / 2);
-                y = sceneView.getTop() + 50;
-            }
-
-
-            if (velocity < MAX_DROP_SPEED){
-                velocity++;
-            }
-
-            if (spriteTouched){
-
-                if ( MAX_JUMP_SPEED < velocity){
-                    velocity = -12;
-                }
-
-                spriteTouched = false;
-            }
-
-            y += velocity;
-
-            currentFrame = (currentFrame + 1) % BMP_COLUMNS;
-        }
-
-        public void draw(Canvas canvas) {
-
-            update();
-
-            int srcX = currentFrame * width;
-            int srcY = 1 * height;
-            Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
-            Rect dst = new Rect(x, y, (x + width / 2) , (y + height / 2));
-            canvas.drawBitmap(bitmap, src, dst, null);
-        }
-
-    }
 
 
 }
